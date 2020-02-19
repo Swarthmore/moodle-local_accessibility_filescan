c238a7b892b5dbf136093208bdd4e28149ddc63c
Andrew Zito <zitoa@lafayette.edu>
2020-02-19 11:00:17 -0500

    Add garbage files to test

--- tests/file_discovery_test.php
+++ tests/file_discovery_test.php
@@ -24,11 +24,15 @@ class local_a11y_assert_file_discovery_testcase extends advanced_testcase {
 
     public function test_file_discovery() {
         $this->resetAfterTest(true);
+        global $DB;
 
         $this->pdfhelper = new \local_a11y_check\pdf();
         $this->task      = new \local_a11y_check\task\find_pdf_files();
         $this->course    = $this->getDataGenerator()->create_course(array('shortname' => 'testcourse'));
         $this->page      = $this->getDataGenerator()->create_module('page', array('course' => $this->course->id));
+
+        $this->add_garbage_files();
+        var_dump($DB->get_records('files'));
 
         $this->add_pdfs(5);
 
@@ -51,6 +55,59 @@ class local_a11y_assert_file_discovery_testcase extends advanced_testcase {
 
         $this->pdfhelper->create_scan_record('thisisafakecontenthash');
         $this->assert_custom_record_count(13);
+    }
+
+    protected function add_garbage_files() {
+        $fs = get_file_storage();
+
+        $garbage = array(
+            (object) array(
+                'contextid' => context_system::instance()->id,
+            ),
+            (object) array(
+                'contextid' => context_user::instance(1)->id,
+            ),
+            (object) array(
+                'contextid' => context_coursecat::instance(1)->id,
+            ),
+            (object) array(
+                'contextid' => context_course::instance($this->course->id)->id,
+            ),
+            (object) array(
+                'mimetype'  => 'application/json',
+            ),
+            (object) array(
+                'component'  => 'assignfeedback_editpdf',
+            ),
+            (object) array(
+                'filearea'  => 'stamps',
+            ),
+        );
+
+        foreach ($garbage as $i => $record) {
+            $uniquetext = (string) time() . (string) $this->filesadded;
+            $record->contextid = $record->contextid ?? context_module::instance($this->page->cmid)->id;
+            $record->component = $record->component ?? 'local_a11y_check';
+            $record->filearea  = $record->filearea ?? 'local_a11y_check_test_files';
+            $record->itemid    = $record->itemid ?? $i;
+            $record->filepath  = $record->filepath ?? '/';
+            $record->filename  = $record->filename ?? 'local_a11y_check_test_file_' . $uniquetext . '.pdf';
+            $record->mimetype  = $record->mimetype ?? 'application/pdf';
+            $fs->create_file_from_string($record, $uniquetext);
+            $this->filesadded++;
+        }
+
+        $record = (object) array(
+            'contextid' => context_module::instance($this->page->cmid)->id,
+            'component' => 'local_a11y_check',
+            'filearea'  => 'local_a11y_check_test_files',
+            'itemid'    => $i,
+            'filepath'  => '/',
+            'filename'  => 'local_a11y_check_test_file_empty.pdf',
+            'mimetype'  => 'application/pdf',
+        );
+        $fs->create_file_from_string($record, '');
+        $this->filesadded++;
     }
 
     protected function add_pdfs($count) {
