752092b5bbe117ece2170e2f9073a6dbba267351
Andrew Zito <zitoa@lafayette.edu>
2020-02-19 14:42:54 -0500

    Update tests and add test for large file

--- tests/local_a11y_check_test.php
+++ tests/local_a11y_check_test.php
@@ -31,7 +31,7 @@ defined('MOODLE_INTERNAL') || die();
  * @copyright 2020 Swarthmore College
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class local_a11y_assert_file_discovery_testcase extends advanced_testcase {
+class local_a11y_check_testcase extends advanced_testcase {
     /** @var \local_a11y_check\pdf The pdf helper object */
     public $pdfhelper;
 
@@ -41,44 +41,70 @@ class local_a11y_assert_file_discovery_testcase extends advanced_testcase {
     /** @var \stdClass the test course object */
     public $course;
 
-    /** @var array the generated pdfs */
-    public $filesadded;
+    /** @var array count of generated pdfs (used to ensure uniqueness) */
+    public $filesaddedcount;
 
-    public function test_file_discovery() {
-        $this->resetAfterTest(true);
-        $this->pdfhelper = new \local_a11y_check\pdf();
-        $this->task      = new \local_a11y_check\task\find_pdf_files();
-        $this->course    = $this->getDataGenerator()->create_course(array('shortname' => 'testcourse'));
-        $this->page      = $this->getDataGenerator()->create_module('page', array('course' => $this->course->id));
+    public function setUp() {
+        $this->resetAfterTest();
+        $this->pdfhelper   = new \local_a11y_check\pdf();
+        $this->task        = new \local_a11y_check\task\find_pdf_files();
+        $this->course      = $this->getDataGenerator()->create_course(array('shortname' => 'testcourse'));
+        $this->page        = $this->getDataGenerator()->create_module('page', array('course' => $this->course->id));
+        $this->filestorage = \get_file_storage();
 
-        $this->add_garbage_files();
+        set_config('maxsize', 10, 'local_a11y_check');
+    }
 
-        $this->add_pdfs(5);
+    public function test_large_file() {
+        global $DB;
 
+        $this->add_pdfs(1, 1000);
+
+        $this->assert_unscanned_files_count(1);
         $this->assert_custom_record_count(0);
-        $this->assert_unscanned_files_count(5);
 
         $this->task->execute();
 
-        $this->assert_custom_record_count(5);
         $this->assert_unscanned_files_count(0);
+        $this->assert_custom_record_count(1);
+
+        $records = $DB->get_records('local_a11y_check_type_pdf');
+        $record  = $records[array_keys($records)[0]];
+
+        $this->assertEquals(LOCAL_A11Y_CHECK_STATUS_IGNORE, $record->status);
+    }
+
+    public function test_find_files() {
+        $this->add_garbage_files();
+
+        $this->assert_unscanned_files_count(0);
+        $this->assert_custom_record_count(0);
+
+        $this->add_pdfs(5);
+
+        $this->assert_unscanned_files_count(5);
+        $this->assert_custom_record_count(0);
+
+        $this->task->execute();
+
+        $this->assert_unscanned_files_count(0);
+        $this->assert_custom_record_count(5);
 
         $this->add_pdfs(7);
 
         $this->assert_unscanned_files_count(7);
+        $this->assert_custom_record_count(5);
 
         $this->task->execute();
 
-        $this->assert_custom_record_count(12);
         $this->assert_unscanned_files_count(0);
+        $this->assert_custom_record_count(12);
     }
 
     /**
      * Create PDFs which should be excluded from the scan.
      */
     protected function add_garbage_files() {
-        $fs = get_file_storage();
-
         $garbage = array(
             (object) array(
                 'contextid' => context_system::instance()->id,
@@ -104,7 +130,7 @@ class local_a11y_assert_file_discovery_testcase extends advanced_testcase {
         );
 
         foreach ($garbage as $i => $record) {
-            $uniquetext = (string) time() . (string) $this->filesadded;
+            $uniquetext = (string) time() . (string) $this->filesaddedcount;
             $record->contextid = $record->contextid ?? context_module::instance($this->page->cmid)->id;
             $record->component = $record->component ?? 'local_a11y_check';
             $record->filearea  = $record->filearea ?? 'local_a11y_check_test_files';
@@ -112,8 +138,8 @@ class local_a11y_assert_file_discovery_testcase extends advanced_testcase {
             $record->filepath  = $record->filepath ?? '/';
             $record->filename  = $record->filename ?? 'local_a11y_check_test_file_' . $uniquetext . '.pdf';
             $record->mimetype  = $record->mimetype ?? 'application/pdf';
-            $fs->create_file_from_string($record, $uniquetext);
-            $this->filesadded++;
+            $this->filestorage->create_file_from_string($record, $uniquetext);
+            $this->filesaddedcount++;
         }
 
         $record = (object) array(
@@ -125,19 +151,20 @@ class local_a11y_assert_file_discovery_testcase extends advanced_testcase {
             'filename'  => 'local_a11y_check_test_file_empty.pdf',
             'mimetype'  => 'application/pdf',
         );
-        $fs->create_file_from_string($record, '');
-        $this->filesadded++;
+        $this->filestorage->create_file_from_string($record, '');
+        $this->filesaddedcount++;
     }
 
     /**
-     * Create the requested number of pdfs.
-     * @param int $count the number of pdfs to create.
+     * Create the requested number of PDFs.
+     * These should all be found by the plugin.
+     *
+     * @param int $count The number of PDFs to create.
+     * @param int $size Force filesize (in bytes).
      */
-    protected function add_pdfs($count) {
-        $fs = get_file_storage();
-
+    protected function add_pdfs($count, $size = null) {
         for ($i = 0; $i < $count; $i++) {
-            $uniquetext = (string) time() . (string) $this->filesadded . (string) $i;
+            $uniquetext = (string) time() . (string) $this->filesaddedcount . (string) $i;
             $record = (object) array(
                 'contextid' => context_module::instance($this->page->cmid)->id,
                 'component' => 'local_a11y_check',
@@ -147,13 +174,19 @@ class local_a11y_assert_file_discovery_testcase extends advanced_testcase {
                 'filename'  => 'local_a11y_check_test_file_' . $uniquetext . '.pdf',
                 'mimetype'  => 'application/pdf',
             );
-            $fs->create_file_from_string($record, $uniquetext);
-            $this->filesadded++;
+
+            if (is_numeric($size)) {
+                $record->filesize = $size;
+            }
+
+            $this->filestorage->create_file_from_string($record, $uniquetext);
+            $this->filesaddedcount++;
         }
     }
 
     /**
      * Wrapper for assertEquals against the local_a11y_check table.
+     *
      * @param int $count the expected number of records.
      *
      * @return boolean
@@ -167,6 +200,7 @@ class local_a11y_assert_file_discovery_testcase extends advanced_testcase {
 
     /**
      * Wrapper for assertEquals against get_unscanned_pdf_files.
+     *
      * @param int $count the expected number of records.
      *
      * @return boolean
