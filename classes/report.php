7ae06d622e91946a4b6c878e2dd7c85ac3eb0373
Anthony Weed <aweed1@swarthmore.edu>
2021-07-08 15:26:15 -0400

    Bumped version Joined table on pathnamehash instead of contenthash

--- classes/report.php
+++ classes/report.php
@@ -40,14 +40,19 @@ class report {
      */
     public static function generate_report($limit = 1000) {
         global $DB;
-        $sql = "SELECT f.scanid, f.contenthash as contenthash,"
+        $sql = "SELECT f.id, f.scanid, f.contenthash as contenthash,"
             . "f.pathnamehash as pathnamehash, f.hastext, f.hastitle, f.haslanguage,"
             . "f.istagged, f.pagecount, f.hasbookmarks, c.status, c.statustext,"
             . "c.lastchecked, files.filename "
             . "FROM {local_a11y_check_type_pdf} f "
             . "INNER JOIN {local_a11y_check} c ON c.id = f.scanid "
-            . "INNER JOIN {files} files ON files.contenthash=f.contenthash";
-        $files = $DB->get_records_sql($sql, null, 0, $limit);
+            . "INNER JOIN {files} files ON files.pathnamehash=f.pathnamehash";
+        $rs = $DB->get_recordset_sql($sql, null, 0, $limit);
+        $files = array();
+        foreach ($rs as $record) {
+          array_push($files, $record);
+        }
+        $rs->close();
         return $files;
     }
 
