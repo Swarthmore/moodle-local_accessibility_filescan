dfb418fed344d58526f349f3c8fc6bb99787f98d
devnoot <devnoot@gmail.com>
2021-09-07 13:58:37 -0400

    Downgraded required Moodle version

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021070900;
-$plugin->requires  = 2021051700;
+$plugin->version   = 2021090700;
+$plugin->requires  = 2020110906;
 $plugin->component = 'local_a11y_check';
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->release   = 'v0.0.1';
