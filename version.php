509c0aae185591c427faae35dd43e0e30fbf3dca
Anthony W. Weed <aweed1@swarthmore.edu>
2023-08-25 16:55:48 -0400

    Bumped version.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 /** @var mixed $plugin */
-$plugin->version   = 2023082203;
+$plugin->version   = 2023082500;
 $plugin->requires  = 2022041909.08;
 $plugin->component = 'local_accessibility_filescan';
 $plugin->maturity  = MATURITY_ALPHA;
