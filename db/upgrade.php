9263517ec6b158bddc9f757b3f7a8ee279c29fbc
Anthony W. Weed <devnoot@gmail.com>
2025-06-10 11:47:16 -0400

    Fixed syntax error

--- db/upgrade.php
+++ db/upgrade.php
@@ -22,13 +22,13 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die
+defined('MOODLE_INTERNAL') || die();
 
 function xmldb_local_accessibility_filescan_upgrade($oldversion) {
 
   global $DB;
 
-  $dbmain = $DB->get_manager();
+  $dbman = $DB->get_manager();
 
   if ($oldversion < 2024050800) {
 
